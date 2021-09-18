751da0525744bd7139c590fee0f20fe53c701f2f
Himanshu Kushwaha <kushwaha0312@gmail.com>
2021-09-19 02:26:47 +0530

    Cheacking Palindrome

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -12,22 +12,29 @@ O/P: 1234 is not a palindrome
  */
 
 
-
-
-import java.util.*;
 public class Palindrome {
-    public static void main(String[] args) {
-        int x = Integer.parseInt(args[0]);
-        int c = x;
+    public int pali;
+    Palindrome(int p)
+    {
+        pali= p;
+    }
+
+    public void Pali_check(){
+        var c = pali;
         var pal =0;
         while(c>0){
             var r=c%10;
             pal = pal*10 +r;
             c=c/10;
         }
-        if(x==pal)
-            System.out.println(x+" is a Palindrome");
+        if(pali == pal)
+            System.out.println(pali+" is a Palindrome");
         else
-            System.out.println(x+" is not a Palindrome");
+            System.out.println(pali+" is not a Palindrome");
+    }
+
+    public static void main(String[] args) {
+        Palindrome obj = new Palindrome(Integer.parseInt(args[0]));
+        obj.Pali_check();
     }
 }
